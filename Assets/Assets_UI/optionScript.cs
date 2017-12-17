04bdc0af9f63cca310a37f3d72472c951c25466d
Vabres <megotenks@gmail.com>
2017-12-17 19:33:49 +0100

    avancement de l'UI

---
+++ Assets/Assets_UI/optionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionScript : MonoBehaviour{
+
+	public GameObject optionMenu;
+
+	public void Start() {
+		hideMenu ();
+	}
+
+	public void deployMenu() {
+		optionMenu.SetActive (true);
+	}
+
+
+	public void hideMenu() {
+		optionMenu.SetActive (false);
+	}
+
+}
