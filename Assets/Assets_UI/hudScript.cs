2f194f0497c90d29679f652d3b9ac0043fc210f9
Vabres <megotenks@gmail.com>
2017-12-21 14:51:38 +0100

    ajout panel alerte

---
+++ Assets/Assets_UI/hudScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class hudScript : MonoBehaviour {
+
+	public Text meatTxt, ratsTxt, bonesTxt, leatherTxt;
+	public int meatInt = 0, ratsInt = 0, bonesInt = 0, leatherInt = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		meatTxt.text = meatInt + "";
+		ratsTxt.text = ratsInt + "";
+		bonesTxt.text = bonesInt + "";
+		leatherTxt.text = leatherInt + "";
+	}
+}
